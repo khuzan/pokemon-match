f622e1521447e0e90bd810fcc4695506e33622cc
khuzan <khuzan557@gmail.com>
2025-01-27 23:18:50 +0800

    change api endpoint

--- src/App.js
+++ src/App.js
@@ -2,54 +2,61 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import SingleCard from "./components/SingleCard";
 
-const cardImages = [
-  { src: "/img/helmet-1.png", matched: false },
-  { src: "/img/potion-1.png", matched: false },
-  { src: "/img/ring-1.png", matched: false },
-  { src: "/img/scroll-1.png", matched: false },
-  { src: "/img/shield-1.png", matched: false },
-  { src: "/img/sword-1.png", matched: false },
-];
+// const cardImages = [
+//   { src: "/img/helmet-1.png", matched: false },
+//   { src: "/img/potion-1.png", matched: false },
+//   { src: "/img/ring-1.png", matched: false },
+//   { src: "/img/scroll-1.png", matched: false },
+//   { src: "/img/shield-1.png", matched: false },
+//   { src: "/img/sword-1.png", matched: false },
+// ];
 
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [cardImages, setCardImages] = useState([]);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
   const [disabled, setDisabled] = useState(false);
+  const [score, setScore] = useState(0);
+  const [energy, setEnergy] = useState(100);
 
-  //get random indices
+  //fetch pokemon api
   const fetchPokemon = async () => {
+    const newArray = [];
     try {
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+      const response = await fetch(
+        "https://api.pokemontcg.io/v2/cards?pageSize=40"
+      );
 
       if (!response.ok) {
         throw new Error("Could not fetch data from API");
       }
-
-      const data = await response.json();
-      const pokemons = data.results;
+      const tcgraw = await response.json();
+      const data = tcgraw.data;
+      const pokemons = [...data].map((d) => ({ ...d, matched: false }));
       getDataSlice(pokemons);
+      
     } catch (err) {
       console.error(err);
     }
   };
 
+  //get pokemon that match from randomIndices
   const getDataSlice = (pokemons) => {
     const randomIndices = getRandomIndices(pokemons);
 
     const dataSlice = randomIndices.map((index) => pokemons[index]);
-    const x  = [...dataSlice]
-    .map((z) => ({ ...z, src: `https://img.pokemondb.net/artwork/${z.name}.jpg` }));
+    setCardImages(dataSlice);
 
-    console.log('slice:', x)
     return dataSlice;
   };
 
+  //get random numbers
   const getRandomIndices = (pokemons) => {
     const randomIndicesArray = [];
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 10; i++) {
       const randomNum = Math.floor(Math.random() * pokemons.length);
       if (!randomIndicesArray.includes(randomNum)) {
         randomIndicesArray.push(randomNum);
@@ -57,12 +64,13 @@ function App() {
         i--;
       }
     }
-    console.log("random", randomIndicesArray);
     return randomIndicesArray;
   };
 
   //shuffle cards
   const shuffleCards = () => {
+    setEnergy(100);
+    setScore(0);
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
@@ -82,18 +90,22 @@ function App() {
   useEffect(() => {
     if (choiceOne && choiceTwo) {
       setDisabled(true);
-      if (choiceOne.src === choiceTwo.src) {
+      if (choiceOne.name === choiceTwo.name) {
         setCards((prevCards) => {
           return prevCards.map((card) => {
-            if (card.src === choiceOne.src) {
+            if (card.name === choiceOne.name) {
+              setEnergy(energy+10)
+              setScore(score+1)
               return { ...card, matched: true };
             } else {
               return card;
             }
           });
         });
+        
         resetTurn();
       } else {
+        setEnergy(energy-20)
         setTimeout(() => resetTurn(), 1000);
       }
     }
@@ -105,17 +117,22 @@ function App() {
     setTurns((prevTurns) => prevTurns + 1);
     setDisabled(false);
   };
-
+  
   useEffect(() => {
-    shuffleCards();
     fetchPokemon();
+    shuffleCards();
   }, []);
 
   return (
     <div className="App">
-      <h1>Magic Match</h1>
+      <h1>Pokemon Match</h1>
       <button onClick={shuffleCards}>New Game</button>
 
+      <div className="card-record">
+        <p>Energy: {energy}%</p>
+        <p>Score: {score}</p>
+        <p>Turns: {turns}</p>
+      </div>
       <div className="card-grid">
         {cards.map((card) => (
           <SingleCard
@@ -127,7 +144,6 @@ function App() {
           />
         ))}
       </div>
-      <p>Turns: {turns}</p>
     </div>
   );
 }
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -10,15 +10,14 @@ export default function SingleCard({ card, handleChoice, flipped, disabled }) {
   return (
     <div className="card">
       <div className={flipped ? "flipped" : ""}>
-        <img className="front" src={card.src} alt="card front" />
+        <img className="front" src={card.images.small} alt="card front" />
         <img
           className="back"
-          src="/img/cover.png"
+          src="/img/pokemon_card_backside.png"
           onClick={handleClick}
           alt="card back"
         />
       </div>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/25.png"></img>
     </div>
   )
 }
